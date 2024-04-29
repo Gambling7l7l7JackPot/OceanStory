7dc651b296d15e936b0d4f7adb214121058d39b4
JNUSYJ <lukai7501@naver.com>
2024-04-29 11:45:31 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OceanStory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
