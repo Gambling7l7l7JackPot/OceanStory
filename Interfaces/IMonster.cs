acde0196397386c843fc0074b5dbd093aa5a4fb3
attiya1125 <leonrey1125@gmail.com>
2024-04-29 17:12:42 +0900

    LJM_IMonster01
    몬스터 기본 시스템 구성01

--- Interfaces/IMonster.cs
+++ Interfaces/IMonster.cs
@@ -8,5 +8,11 @@ namespace OceanStory.Interfaces
 {
     internal interface IMonster
     {
+        string Name { get; }
+        int Hp { get; set; }
+        int Level { get; }
+        int Atk { get; }
+        bool MosterDead { get; }
+        void GetDamage (int damage);
     }
 }
