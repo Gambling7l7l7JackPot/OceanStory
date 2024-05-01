0640753d00e4cadd16280f1554b550d48661890b
Yoonwoojoo <yoonkibum@naver.com>
2024-05-01 20:39:03 +0900

    YKB_equipment
    아이템 장착 시 [E]뜨고 해제하면 [E] 사라짐
    
    < 필요한 작업 >
    장착한 아이템을 캐릭터 능력치에 적용 및 같은 부위 아이템 중복착용을 금지, 그리고 소모성 아이템일 경우에도 고려하여 추가로직 요망

--- Inventory.cs
+++ Inventory.cs
@@ -10,7 +10,10 @@ namespace OceanStory
     internal class Inventory
     {
         List<Item> itemList = new List<Item>();
-        List<Item> equipItems = new List<Item>();
+        List<Item> equipItem = new List<Item>();
+
+        string infoAtk = "  공격력";
+        string infoDef = "  방어력";
 
         public Inventory()
         {
@@ -29,14 +32,12 @@ namespace OceanStory
 
                 for (int i = 0; i < itemList.Count; i++)
                 {
-                    string infoAtk = "  공격력";
-                    string infoDef = "  방어력";
-                    string equipMark = equippedItems.Contains(itemList[i]) ? "[E]" : ""; // 장착시킨건 인벤에서 뜨도록 
-                    Console.WriteLine($"{itemList[i].Name} ({itemList[i].Description}){(itemList[i].Atk != null ? infoAtk + " " + itemList[i].Atk : "")}{(itemList[i].Def != null ? infoDef + " " + itemList[i].Def : "")}");
+                    string equipMark = equipItem.Contains(itemList[i]) ? "[E] " : ""; // 장착중이면 [E]표시 , 노장착중이면 아무런표시 없음
+                    Console.WriteLine($"{equipMark}{itemList[i].Name} ({itemList[i].Description}){(itemList[i].Atk != null ? infoAtk + " " + itemList[i].Atk : "")}{(itemList[i].Def != null ? infoDef + " " + itemList[i].Def : "")}");
                 }
 
                 Console.WriteLine("\n\n0. 돌아가기");
-                Console.WriteLine("1. 아이템")
+                Console.WriteLine("1. 아이템 관리");
          
                 switch (Program.SceneManager.GetUserInput(1))
                 {
@@ -56,7 +57,39 @@ namespace OceanStory
         {
             while (true)
             {
-                Console.WriteLine("장비창");
+                Console.Clear();
+                Console.WriteLine("\n관리하려는 아이템을 선택하세요\n");
+
+                for(int i = 0; i < itemList.Count; i++)
+                {
+                    string equipMark = equipItem.Contains(itemList[i]) ? "[E] " : ""; // 장착중이면 [E]표시 , 노장착중이면 아무런표시 없음
+                    Console.WriteLine($"{i+1}. {equipMark}{itemList[i].Name} ({itemList[i].Description}){(itemList[i].Atk != null ? infoAtk + " " + itemList[i].Atk : "")}{(itemList[i].Def != null ? infoDef + " " + itemList[i].Def : "")}");
+                }
+                Console.WriteLine("\n\n0. 돌아가기\n");
+
+                int input = Program.SceneManager.GetUserInput(itemList.Count);
+
+                if (input == 0)
+                {
+                    return ;
+                }
+                else if (input >= 1 && input <= itemList.Count)
+                {
+                    Item selected = itemList[input-1];
+
+                    if (equipItem.Contains(selected)) // 만약 이미 장착된 아이템을 입력한 것이라면
+                    {
+                        equipItem.Remove(selected); // 장착된 아이템이면 해제
+                    }
+                    else // 장착된 아이템을 입력한것이 아니었다면
+                    {
+                        equipItem.Add(selected); // 해당 아이템을 장착해라
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                }
             }
         }
     }
