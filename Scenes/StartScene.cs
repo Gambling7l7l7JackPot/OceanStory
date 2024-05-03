a61375342e42d9e7145565f1f367c98ce28c4b4f
attiya1125 <leonrey1125@gmail.com>
2024-05-03 17:33:46 +0900

    LJM_BugFix_02

--- Scenes/StartScene.cs
+++ Scenes/StartScene.cs
@@ -10,8 +10,8 @@
             {
                 Console.Clear();
                 Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.");
-                Console.WriteLine("\n1. 상태 보기\n2. 전투 시작\n3. 회복\n4. 인벤토리\n5. 퀘스트\n\n0. 게임 종료");
-                int input = Program.SceneManager.GetUserInput(5);
+                Console.WriteLine("\n1. 상태 보기\n2. 전투 시작\n3. 회복\n4. 인벤토리\n5. 퀘스트\n6. 저장하기\n\n0. 게임 종료");
+                int input = Program.SceneManager.GetUserInput(6);
                 switch (input)
                 {
                     // 상태 보기
